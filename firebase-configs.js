60e62c9c0134fa6866aa3d9471b37e786e537560
Artem Malichev <amalichev@intelliants.com>
2020-03-31 23:25:57 +0600

    Test

---
+++ firebase-configs.js
@@ -0,0 +1,10 @@
+firebaseConfigs = {
+  apiKey: "AIzaSyBKrnnJT9TQUTzkCeP6j0Rd9MbwTWYjDoo",
+  authDomain: "belliata-289ee.firebaseapp.com",
+  databaseURL: "https://belliata-289ee.firebaseio.com",
+  projectId: "belliata-289ee",
+  storageBucket: "belliata-289ee.appspot.com",
+  messagingSenderId: "243719107892",
+  appId: "1:243719107892:web:0584e35ec82ec1cea2a161",
+  measurementId: "G-9TGPZ5C10P"
+};
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-messaging.js');
-//importScripts('/node_modules/firebase/firebase-app.js');
-//importScripts('/node_modules/firebase/firebase-messaging.js');
+importScripts('/firebase-configs.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBKrnnJT9TQUTzkCeP6j0Rd9MbwTWYjDoo",
-  authDomain: "belliata-289ee.firebaseapp.com",
-  databaseURL: "https://belliata-289ee.firebaseio.com",
-  projectId: "belliata-289ee",
-  storageBucket: "belliata-289ee.appspot.com",
-  messagingSenderId: "243719107892",
-  appId: "1:243719107892:web:0584e35ec82ec1cea2a161",
-  measurementId: "G-9TGPZ5C10P"
-});
+firebase.initializeApp(firebaseConfigs);
 
 const messaging = firebase.messaging();
-
-messaging.getToken().then((currentToken) => {
-  console.log('Token:', currentToken);
-}).catch((error) => {
-  console.log(error);
-});
