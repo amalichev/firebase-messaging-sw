37005ecb050984ca2fb34c2bc0d48a1d2ccdd3b6
Artem Malichev <amalichev@intelliants.com>
2020-04-01 22:37:26 +0600

    sw.js changed

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-//importScripts('/node_modules/workbox-sw/build/workbox-sw.js');
 
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
